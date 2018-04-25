791f1c75e4e5fe8764a9399285657a0768995c75
WeiHo <6015125@qq.com>
2018-04-25 20:47:22 +0800

    weiho
    blog

--- static/js/top.js
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript"> 
-function form_top(){ 
-document.write('<div id="form_top"><a href="#_top" title="回到顶部"><div class="top_img"></div></a></div>') 
-} 
-form_top(); 
-</script> 
