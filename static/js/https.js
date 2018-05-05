01dc6b27cc246cdf81ba1fabd54b2b2c230c8057
WeiHo <6015125@qq.com>
2018-05-05 18:21:35 +0800

    https
    site update:2018-05-05 18:21

--- static/js/https.js
+++ static/js/https.js
@@ -3,9 +3,9 @@ Author:Weiho
 Time:2018-05-05 18:17
 https://github.com/zhaoweiho
 */
-(function () {
+(function($){
 var targetProtocol = "https:";
 if (window.location.protocol != targetProtocol)
 window.location.href = targetProtocol +
 window.location.href.substring(window.location.protocol.length);
-        }
+})
